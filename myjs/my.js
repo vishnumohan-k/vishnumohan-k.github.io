a1db399ef0206a3c2a6ab0a36197a1a5a888831e
vishnumohan-k <vishnumohan15@gmail.com>
2016-08-05 11:13:31 +0530

    third

---
+++ myjs/my.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+    $('#content').load('slider.html').fadeIn('slow');
+    })
+
+function products(){
+    $('#content').load('slider.html').fadeIn('slow');
+
+	}
+function contact(){
+    $('#content').load('Contact.html').fadeIn('slow');
+
+    }
+function resume(){
+    $('#content').load('slider.html').fadeIn('slow');
+
+    }
